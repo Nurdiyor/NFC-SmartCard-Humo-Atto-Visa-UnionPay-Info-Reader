05748c3c013abf6cf801e61256ee6fc5a20065fa
Jakhongir <jakhongir.jalilov@gmail.com>
2021-04-03 17:05:32 +0500

    added visa reader

--- app/src/main/java/uz/micro/star/nfcsmartcardinforeader/MainActivity.kt
+++ app/src/main/java/uz/micro/star/nfcsmartcardinforeader/MainActivity.kt
@@ -1,29 +1,52 @@
 package uz.micro.star.nfcsmartcardinforeader
 
 import android.content.Intent
+import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.peerbits.creditCardNfcReader.CardNfcAsyncTask
+import com.peerbits.creditCardNfcReader.utils.CardNfcUtils
 import uz.micro.star.humo_atto_visa_union_pay_nfc_info_reader.nfc.CardNfcManager
 import uz.micro.star.humo_atto_visa_union_pay_nfc_info_reader.nfc.SPEC
 import uz.micro.star.humo_atto_visa_union_pay_nfc_info_reader.nfc.bean.Card
 import uz.micro.star.nfcsmartcardinforeader.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CardNfcAsyncTask.CardNfcInterface {
     private lateinit var binding: ActivityMainBinding
     lateinit var mCardNfcManager: CardNfcManager
     private var mSafeExit = false
+
+    private var mCardNfcAsyncTask: CardNfcAsyncTask? = null
+    private var mNfcAdapter: NfcAdapter? = null
+    private var mIntentFromCreate = false
+
+    private var card: String? = null
+    private var cardType: String? = null
+    private var expiredDate: String? = null
+    private var mIsScanNow = false
+    private var mCardNfcUtils: CardNfcUtils? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        if (mNfcAdapter == null) {
+            /* TextView noNfc = findViewById(android.R.id.candidatesArea);
+            noNfc.setVisibility(View.VISIBLE);*/
+            // Shahil
+        } else {
+            mCardNfcUtils = CardNfcUtils(this)
+            mIntentFromCreate = true
+//            onNewIntent(intent)
+        }
         mCardNfcManager =
             CardNfcManager(
                 this
             )
-//        getNFCInfo()
         onNewIntent(intent)
     }
 
@@ -40,17 +63,12 @@ class MainActivity : AppCompatActivity() {
             }) {
             Toast.makeText(this, "Xatolik !!!!", Toast.LENGTH_SHORT).show()
         }
+        if (mNfcAdapter != null && mNfcAdapter!!.isEnabled) {
+            mCardNfcAsyncTask = CardNfcAsyncTask.Builder(this, intent, mIntentFromCreate)
+                .build()
+        }
     }
 
-//    override fun onReadEvent(event: SPEC.EVENT?, vararg objs: Any?) {
-//        if (event == SPEC.EVENT.IDLE) {
-////            showProgressBar();
-//        } else if (event == SPEC.EVENT.FINISHED) {
-////            hideProgressBar();
-//            val card: Card? = if (objs.isNotEmpty()) objs[0] as Card? else null
-//            buildResult(card)
-//        }
-//    }
 
     private fun buildResult(card: Card?) {
         if (card != null && !card.hasReadingException()) {
@@ -60,7 +78,7 @@ class MainActivity : AppCompatActivity() {
                 binding.nfcInfo.text =
                     card.serialAndDate.cardNumber + "\n" + card.serialAndDate.cardExpiredDate
                 Toast.makeText(this, binding.nfcInfo.text.toString(), Toast.LENGTH_SHORT).show()
-                Log.d("SERIAL", "buildResult: " +  binding.nfcInfo.text.toString())
+                Log.d("SERIAL", "buildResult: " + binding.nfcInfo.text.toString())
             }
         } else {
 //            ret.putExtra(RET,NfcReaderApplication.getStringResource(R.string.info_nfc_error));
@@ -91,6 +109,83 @@ class MainActivity : AppCompatActivity() {
             Handler().postDelayed({ mSafeExit = true }, 800)
         } else {
             mSafeExit = false
+        }
+    }
+
+    override fun startNfcReadCard() {
+        Toast.makeText(this, "started", Toast.LENGTH_SHORT).show()
+        mIsScanNow = true
+    }
+
+    override fun cardIsReadyToRead() {
+        Toast.makeText(this, "reading", Toast.LENGTH_SHORT).show()
+
+        // mCardReadyContent.setVisibility(View.VISIBLE);
+        card = mCardNfcAsyncTask?.cardNumber
+        card = getPrettyCardNumber(card!!)
+        expiredDate = mCardNfcAsyncTask?.cardExpireDate
+        cardType = mCardNfcAsyncTask?.cardType
+//        val cardHolderFirstName: String = mCardNfcAsyncTask?.cardFirstName!!
+//        val cardHolderLastName: String = mCardNfcAsyncTask?.cardFirstName!!
+        val cardCvv: Int = mCardNfcAsyncTask?.cardCvv!!
+        binding.nfcInfo.text =
+            card + "\n" + expiredDate + "\n" + cardType
+
+
+        /*   mPutCardContent.setVisibility(View.GONE);
+        mCardNumberText.setText(card + cardType);
+        mExpireDateText.setText(expiredDate);*/
+        // Shahil
+        Log.e(
+            "Details",
+            "-->$card -->$cardType-->$expiredDate--> $cardCvv"
+        )
+        parseCardType(cardType!!)
+    }
+
+    override fun doNotMoveCardSoFast() {
+
+    }
+
+    override fun unknownEmvCard() {
+
+    }
+
+    override fun cardWithLockedNfc() {
+
+    }
+
+    override fun finishNfcReadCard() {
+        Log.d("ZZZ", "finished")
+        Toast.makeText(this, "finished", Toast.LENGTH_SHORT).show()
+        mCardNfcAsyncTask = null
+        mIsScanNow = false
+    }
+
+    private fun getPrettyCardNumber(card: String): String {
+        val div = " - "
+        return (card.substring(0, 4) + div + card.substring(4, 8) + div + card.substring(8, 12)
+                + div + card.substring(12, 16))
+    }
+
+    private fun parseCardType(cardType: String) {
+        when (cardType) {
+            CardNfcAsyncTask.CARD_UNKNOWN -> {
+                Toast.makeText(
+                    this,
+                    "unknown",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            CardNfcAsyncTask.CARD_VISA -> {
+                //   mCardLogoIcon.setImageResource(R.mipmap.visa_logo); Shahil
+            }
+            CardNfcAsyncTask.CARD_MASTER_CARD -> {
+                //   mCardLogoIcon.setImageResource(R.mipmap.master_logo); Shahil
+            }
+            CardNfcAsyncTask.CARD_HUMO -> {
+                Toast.makeText(this, "HUMO READING", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
